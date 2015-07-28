8665138417988fa2f70440899baef1c5f479983b
Sara Morais <saracristinasm@gmail.com>
2015-07-27 20:12:38 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 
 if (process.env.HEROKU_POSTGRESQL_d6jb0stgq34feh_URL) {
